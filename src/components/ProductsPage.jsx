b57a74e415161079dcb7f510316814fc4e74a196
FlyerBird <99897631+FlyerBird@users.noreply.github.com>
2022-07-29 11:54:50 +0200

    it1Done

---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import jsonData from '../data.json';
+import SearchBar from './SearchBar';
+import ProductTable from './ProductTable';
+
+export default function ProductPage() {
+    const [products, setProducts] = useState(jsonData);
+  return (
+    <div>
+        <h1>IronStore</h1>
+        <div>
+          <SearchBar/>
+        </div>
+
+        <div>
+          <ProductTable/>
+        </div>
+      </div> 
+  )
+}
